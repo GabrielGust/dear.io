d549b7503dd7f7b7b873cec610cedb1cf02b71ab
GabrielGust <gabrielaraujotinoco@gmail.com>
2020-12-11 17:09:15 -0300

    Update in UserDB

--- classes/userdb.php
+++ classes/userdb.php
@@ -21,25 +21,5 @@ class UserDB
     $read = $this->connection->prepare("select * from Users where email= ?");
     $read->execute([$email]);
     return $read->fetchAll(PDO::FETCH_OBJ);
-  }
-
-  public function updateUser($email, $user_password, $id_user){
-    $update = $this->connection->prepare("update Users set email = :email, user_password = :password where id_user = :id");
-
-    $update->bindValue(":email", $email, PDO::PARAM_STR);
-    $update->bindValue(":password", $user_password, PDO::PARAM_STR);
-    $update->bindValue(":id", $id_user, PDO::PARAM_INT);
-    $update->execute();
-
-    echo 'Usuário atualizado com sucesso!';
-  }
-
-  public function deleteUser($id_user)
-  {
-    $delete = $this->connection->prepare("delete from Users where id_user = :id");
-    $delete->bindValue(":id", $id_user, PDO::PARAM_INT);
-    $delete->execute();
-
-    echo 'Usuário excluído com sucesso!';
-  }
+  }s
 }
